44766a8537149403a382159798b2a2ae3c77bc03
mathewb <mathew.bryan@radiuspaymentsolutions.com>
2020-01-27 13:12:32 +0000

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const AboutMe = () => {
+    return(
+        <div>
+        <h1>
+            ABOUT ME
+        </h1>
+        </div>
+    )
+}
+
+export default AboutMe
